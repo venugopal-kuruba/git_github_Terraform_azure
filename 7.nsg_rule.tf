7d75cb62f6b1705036d837271b1abd216c6707af
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-08-28 10:33:45 +0530

    add the source

--- 7.nsg_rule.tf
+++ 7.nsg_rule.tf
@@ -6,8 +6,8 @@ resource "azurerm_network_security_rule" "tcp22" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  source_address_prefix       = "57.48.115.33/32"
+  destination_address_prefix  = "10.42.1.0/24"
   resource_group_name         = azurerm_resource_group.devsecopsb42-rg1.name
   network_security_group_name = azurerm_network_security_group.nsg1.name
 }
